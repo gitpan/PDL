6d264939460666d60dfae41b2a148d6ec89821cf
Chris Marshall <chm@cpan.org>
2010-05-31 16:48:34 +0000

    Import of CHM/PDL-2.4.6_008 from CPAN.
    gitpan-cpan-distribution: PDL
    gitpan-cpan-version:      2.4.6_008
    gitpan-cpan-path:         CHM/PDL-2.4.6_008.tar.gz
    gitpan-cpan-author:       CHM
    gitpan-cpan-maturity:     developer

---
+++ Graphics/TriD/OpenGL/X.h.cpp
@@ -0,0 +1,27 @@
+/* command: gcc -E -P -DGLAPI=extern -DGL_MESA_program_debug -DGL_MESA_shader_debug -DGL_ATI_blend_equation_separate -DGLX_MESA_swap_frame_usage -DGLX_MESA_swap_control -DGLX_ARB_render_texture -DPERL_USE_SAFE_PUTENV -U__STRICT_ANSI__ -fno-strict-aliasing -pipe -I/usr/local/include -DPERL_USE_SAFE_PUTENV -U__STRICT_ANSI__ -fno-strict-aliasing -pipe -I/usr/local/include -D_LANGUAGE_C -DAPIENTRY='' */
+typedef unsigned long XID;
+
+
+
+typedef unsigned long Mask;
+
+
+
+typedef unsigned long Atom;
+
+typedef unsigned long VisualID;
+typedef unsigned long Time;
+typedef XID Window;
+typedef XID Drawable;
+
+
+typedef XID Font;
+
+typedef XID Pixmap;
+typedef XID Cursor;
+typedef XID Colormap;
+typedef XID GContext;
+typedef XID KeySym;
+
+typedef unsigned char KeyCode;
+Start of /usr/include/X11/X.h (23Cnba1nbf31)....
