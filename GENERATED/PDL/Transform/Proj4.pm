05ff46aa1965c99233d383b1dd6402370e9a4440
Chris Marshall <chm@cpan.org>
2015-01-06 12:05:39 +0000

    Import of CHM/PDL-2.007_07.tar.gz from CPAN.
    gitpan-cpan-distribution: PDL
    gitpan-cpan-version:      2.007_07
    gitpan-cpan-path:         CHM/PDL-2.007_07.tar.gz
    gitpan-cpan-author:       CHM
    gitpan-cpan-maturity:     developer

---
+++ GENERATED/PDL/Transform/Proj4.pm
@@ -0,0 +1,36 @@
+
+#
+# GENERATED WITH PDL::PP! Don't modify!
+#
+package PDL::Transform::Proj4;
+
+@EXPORT_OK  = qw( );
+%EXPORT_TAGS = (Func=>[@EXPORT_OK]);
+
+use PDL::Core;
+use PDL::Exporter;
+use DynaLoader;
+
+
+
+   
+   @ISA    = ( 'PDL::Exporter','DynaLoader' );
+   push @PDL::Core::PP, __PACKAGE__;
+   bootstrap PDL::Transform::Proj4 ;
+
+
+
+
+
+
+
+
+;
+
+
+
+# Exit with OK status
+
+1;
+
+		   
